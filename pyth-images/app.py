b23b7502612837ad8a97be09c2d426b2c4245781
satishchippa-bob <satishchippa10@gmail.com>
2024-11-19 09:12:13 +0530

    file

--- pyth-images/app.py
+++ pyth-images/app.py
@@ -1,5 +1,18 @@
-import time
-while True:
-    time.sleep(1)
+from flask import Flask
 
-print("hello python")
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return "Hello, world from Flask!"
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
+    
+    
+    
+    
+    # import time
+#while True:#    time.sleep(1)
+
+#print("hello python")
