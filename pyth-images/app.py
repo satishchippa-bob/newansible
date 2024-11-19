b6fc717db965c3267e0d65c962143f0e1261c921
satishchippa-bob <satishchippa10@gmail.com>
2024-11-19 10:30:07 +0530

    file

--- pyth-images/app.py
+++ pyth-images/app.py
@@ -10,11 +10,11 @@ from flask import Flask
 app = Flask(_name_)
 
 @app.route('/')
-def home():
-    return "Hello, world from Flask!"
+def hello_world():
+    return "Hello_world!"
 
-if _name_ == '_main_':
-    app.run(host='0.0.0.0', port=5000)
+#if _name_ == '_main_':
+#    app.run(host='0.0.0.0', port=5000)
     
     
     
